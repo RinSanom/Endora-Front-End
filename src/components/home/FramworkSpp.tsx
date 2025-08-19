bf4d0bb06d12992e1bae22205e1275d2bad9f333
RinSanom <rinsanom3@gmail.com>
2025-08-19 09:13:46 +0700

    [Fix] Fix Build

--- src/components/home/FramworkSpp.tsx
+++ src/components/home/FramworkSpp.tsx
@@ -84,8 +84,8 @@ export default function FrameworkSection() {
             </h2>
             <p className="text-xl text-gray-300 dark:text-gray-400 max-w-3xl mx-auto">
               Pixel supports a wide range of front-end frameworks, allowing you
-              to build your projects with the tools you love. Whether you're a
-              fan of
+              to build your projects with the tools you love. Whether
+              you&apos;re a fan of
               {hoveredFramework && (
                 <span className="text-secondary ml-2 transition-opacity duration-300">
                   {hoveredFramework}
